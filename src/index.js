31471b9fc1c1eea6b9527c15078209ca98f57cfd
Serhii Onyshchenko <vkhellboy1992@gmail.com>
2022-04-04 10:13:30 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,35 +1,37 @@
 import './css/styles.css';
 import Notiflix from 'notiflix';
-import API from './js/fetchCountries'
+import API from './js/fetchCountries';
 
 var debounce = require('lodash.debounce');
 const DEBOUNCE_DELAY = 300;
-const ref={
-	inputEL: document.querySelector('#search-box'),
-	ulEl: document.querySelector('.country-list'),
-	divInfoEl: document.querySelector('.country-info'),
-}
+const ref = {
+  inputEL: document.querySelector('#search-box'),
+  ulEl: document.querySelector('.country-list'),
+  divInfoEl: document.querySelector('.country-info'),
+};
 
-ref.inputEL.addEventListener('input', debounce(sorcheContry, DEBOUNCE_DELAY))
+ref.inputEL.addEventListener('input', debounce(sorcheContry, DEBOUNCE_DELAY));
 
 function sorcheContry(e) {
-		const name = e.target.value
-	if (name.length){
-	API.fetchCountries(name)
-	.then(renderListContry)
-	.catch(onFetchError)
-}}
-
-
-
+	const name = e.target.value.trim();
+  if (!name.length) {
+    ref.ulEl.innerHTML = '';
+  }
+  if (name.length) {
+    API.fetchCountries(name).then(renderListContry).catch(onFetchError);
+  }
+}
 function renderListContry(contrys) {
-	if (contrys.length>10) {
-		return	Notiflix.Notify.info("Too many matches found. Please enter a more specific name.")
-	}
-	if(contrys.length===1){
-		const markup = contrys.map(contry=>{
-			const {flags,name,population,capital,languages}=contry
-			return	`<div class="card-icon-name">
+  if (contrys.length > 10) {
+    ref.ulEl.innerHTML = '';
+    ref.divInfoEl.innerHTML = '';
+    return Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+  }
+  if (contrys.length === 1) {
+    const markup = contrys
+      .map(contry => {
+        const { flags, name, population, capital, languages } = contry;
+        return `<div class="card-icon-name">
 				<img class="card-icon" width="30"  src="${flags.svg}" alt="${name}">
 				<h2 class="card-name">${name.common}</h2>
 			</div>
@@ -38,22 +40,27 @@ function renderListContry(contrys) {
 				<p class="card-text"><span>Population:</span> ${population}</p>
 				<p class="card-text"><span>Languages:</span> ${Object.values(languages)}
 				</p>
-			</div>`
-			}).join(" ")
-		ref.ulEl.innerHTML = ""
-		return	ref.divInfoEl.innerHTML = markup	
-	}
-	const markup = contrys.map(contry=>{
-		const {flags,name}=contry
-		return	`<li class="item-cantry">
+			</div>`;
+      })
+      .join(' ');
+    ref.ulEl.innerHTML = '';
+    return (ref.divInfoEl.innerHTML = markup);
+  }
+  const markup = contrys
+    .map(contry => {
+      const { flags, name } = contry;
+      return `<li class="item-cantry">
 		<img width="30"  src="${flags.svg}" alt="${name}">
 		${name.common}
-		</li>`
-		}).join(" ")
-	ref.divInfoEl.innerHTML = ""
-	ref.ulEl.innerHTML = markup
+		</li>`;
+    })
+    .join(' ');
+  ref.divInfoEl.innerHTML = '';
+  ref.ulEl.innerHTML = markup;
 }
 
-function onFetchError(error) {
-		Notiflix.Notify.failure("Oops, there is no country with that name")
+function onFetchError() {
+  Notiflix.Notify.failure('Oops, there is no country with that name');
+  ref.ulEl.innerHTML = '';
+  ref.divInfoEl.innerHTML = '';
 }
